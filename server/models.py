c8bc1fd6865e5ef09b99e2967f634c60f5cfaa8c
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-10 06:54:51 +0300

    define the user columns

--- server/models.py
+++ server/models.py
@@ -6,3 +6,15 @@ db = SQLAlchemy()
 
 class User(db.Model, SerializerMixin):
     _tablename_ = 'users'
+
+    serialize_rules = ('-password',)
+    
+    id = db.Column(db.Integer,primary_key = True)
+    first_name =db.Column(db.String,nullable = False)
+    last_name  = db.Column (db.String,nullable = False)
+    phone_number = db.Column(db.String,nullable = False ,unique=True)
+    password = db.Column (db.VARCHAR ,nullable = False)
+    profile_photo = db.Column (db.VARCHAR ,nullable = True)
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
+
+    
