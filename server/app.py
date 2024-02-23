0626c40ea39f5e1e0c2544cf912dfa414f9f7519
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-23 11:51:02 +0300

    create database

--- server/app.py
+++ server/app.py
@@ -134,3 +134,17 @@
 
 # if __name__ == "_main_":
 #     app.run(port=5555,debug=True)
+
+from flask import Flask
+from models import db
+from flask_migrate import Migrate
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///messaging.db'
+db.init_app(app)
+migrate=Migrate(app, db)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
