e4a0713a6be359bcf66e85a38b9a11ffc1744024
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-23 12:44:02 +0300

    create a route to create a new user

--- server/controllers/users.py
+++ server/controllers/users.py
@@ -10,7 +10,16 @@ class Users(Resource):
             users_list=[user.to_dict() for user in users]
             return make_response(jsonify(users_list), 200)
         return make_response(jsonify({"error":"users not found"}), 404)
-    
+    def post(self):
+        data=request.get_json()
+        existing_user=User.query.filter_by(email=data['email']).first()
+        if existing_user:
+            abort(409, detail="User already exists")
+        user=User(firstname=data['firstname'], lastname=data['lastname'], email=data['email'], password=data['password'], phone_no=data['phone_no'])
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return make_response(jsonify({"message":"user created successfully"}), 201)
+        except Exception as e:
+            return make_response(jsonify({"error":"error creating the user"}), 404)
             
-
-
